64efed330d41595baca1853c4ec1a7f88e45a769
Kevin Smith <kevin.ar.smith@gmail.com>
2021-04-25 07:05:31 -0500

    changes

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,19 +1,17 @@
 // from data.js get the data (Starter code given)
 var tableData = data;
-
-// Create / collect the column data
-var Columns= ["datetime", "city","state","country","shape", "durationMinutes", "comments"]
-
-// Select the Button
-var Button=d3.select("#filter-button");
+console.log(tabledata);
 
 // Select the body
 var $tbody=d3.select ("tbody");
+console.log(data)
 
 // Reference the Field Date and city
 var inputFieldDate = d3.select("#datetime");
 var inputFieldCity = d3.select("#city");
 
+//select the HTML element for entry
+var datainput = (datainput) ==> 
 
 
 
