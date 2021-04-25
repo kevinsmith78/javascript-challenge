6ea4a521ae2cc7f1657e3ea9cbd2e3d80148d56a
Kevin Smith <kevin.ar.smith@gmail.com>
2021-04-25 08:56:46 -0500

    more button

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,10 +3,19 @@ var tableData = data;
 console.log(tabledata);
 
 //Select the button
-var button = d3.select("#filter-button");
+var button = d3.select("#filter-btn");
 
 //Create the event handler
 button.on"click",() => {
+    //Prevent the page from refreshing
+    d3.event.preventDefault();
+    tbody.hmtl("");
+    //Select the input element and get the raw HTML node
+    var inputDate = inputfieldDate.property("value");
+    //console logging for sanity
+    console.log(inputDate)
+    //filter data to run through every fiter data aray
+    var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
 
 }
 
@@ -17,7 +26,7 @@ var tbody=d3.select ("tbody");
 console.log(data);
 
 //Use d3 to update each cells's text with alien report values
-data.forEach(function(ufoSighting);{
+data.forEach(function(ufoSighting){
     console.log(ufosighting);
     var row=tbody.append("tr");
     Object.defineProperties(ufoSighting).forEach(function([key,value]){
