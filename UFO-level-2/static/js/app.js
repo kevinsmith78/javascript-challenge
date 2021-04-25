a7d42f3a07a9792fc53447a7462d493897cce1c5
Kevin Smith <kevin.ar.smith@gmail.com>
2021-04-25 16:57:36 -0500

    index things

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,3 +1,11 @@
 //from data.js get the data (Starter code given)
-var tableData = data;
+var ufo = data;
 console.log(tabledata);
+
+//create the variables
+var $tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputFieldDate = d3.select("#datetime");
+var inputFieldCity = d3.select("#city");
+var columns = ["datetime","city", "state","country","shape","durationMinutes","comments"]
+
