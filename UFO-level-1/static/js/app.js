e6572572a9e092b6e3f49fff4369f34e31ee495a
Kevin Smith <kevin.ar.smith@gmail.com>
2021-04-25 11:11:52 -0500

    modification based on instruction

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -11,17 +11,27 @@ button.on("click",function(event){
     d3.event.preventDefault();
     tbody.hmtl("");
     //Select the input element and get the raw HTML node
-var inputElement = d3.select("#datetime");
-var inputValue = inputElement.property("value");
-    //console logging for sanity
+   
     
-    //filter data to run through every fiter data aray
-var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
-filteredData.forEach(function(dateData){
+    //filter data to run through every fiter data aray filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+data.forEach(observation =>{
+    //output the message to the web console (removed to object.entries)
+    //add the rows to the table
+    var row=tbody.append("tr");
+    //use 'Object.entries' to console.log each report
+    Object.entries(observation).forEach([key,value]) => {
+        console.log(key,value)
+    //append 1 cell per weather report
+        var cell = row.append("td")
+        cell.text(value)
+     })
+});
     var row=tbody.append("tr");
     Object.entiries(dateData).forEach(function([key,value]){
-        console.log(key,value);
-//Append a cell to the row for each value in the ufo sighting object
+    console.log(key,value);
+
+
+        //Append a cell to the row for each value in the ufo sighting object
         var row = tbody.append("tr");
         Object.entries(dateData).forEach(function([key,value]){
         var cell=tbody.append("td");
